caadcc5f248b8a5204de824404a00caaaea9a93f
K11BN367 <alexander592beckmann@gmail.com>
2024-07-11 14:26:03 +0200

    initial commit

--- src/'Argument'Dependency'Value'Union'Constructor'Interface'Global'Macro'/URI/'Macro'1.jl
+++ src/'Argument'Dependency'Value'Union'Constructor'Interface'Global'Macro'/URI/'Macro'1.jl
@@ -1,18 +1,18 @@
 
 macro c__URI(Value, Tuple...)
-    Path_1,   Tuple = SoftBase.unpack_arguments(SoftBase.a__Path(""), SoftBase.eval.(Tuple)...)
     Path_2 = Base.String(__source__.file)
-    return quote
-        
+
+    return quote    
+        Path_1,   Tuple = SoftBase.unpack_arguments(SoftBase.a__Path(""), SoftBase.eval.($Tuple)...)
         if $Value == SoftBase.File
             Return = SoftBase.join_path(
                 c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path($Path_2)),
-                c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path($Path_1))
+                c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path(Path_1))
             )
         elseif $Value == SoftBase.Directory
             Return = SoftBase.join_path(
                 c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path(Base.String(Base.dirname($Path_2)))),
-                c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path($Path_1))
+                c__URI(SoftBase.a__Scheme("file"), SoftBase.a__Path(Path_1))
             )
         end
         Return
